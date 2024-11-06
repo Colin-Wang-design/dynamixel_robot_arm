c61c96f8780ea2319a391cb8651f969fcefa6511
s232909 <s232909@student.dtu.dk>
2024-11-06 12:01:30 +0100

    initial commit

---
+++ project.py
@@ -0,0 +1,43 @@
+from dynamixel_sdk import * 
+
+ADDR_MX_TORQUE_ENABLE = 24
+ADDR_MX_CW_COMPLIANCE_MARGIN = 26
+ADDR_MX_CCW_COMPLIANCE_MARGIN = 27
+ADDR_MX_CW_COMPLIANCE_SLOPE = 28
+ADDR_MX_CCW_COMPLIANCE_SLOPE = 29
+ADDR_MX_GOAL_POSITION = 30
+ADDR_MX_MOVING_SPEED = 15
+ADDR_MX_PRESENT_POSITION = 36
+ADDR_MX_PUNCH = 48
+PROTOCOL_VERSION = 1.0
+DXL_IDS = [1,2,3,4]
+DEVICENAME = '/dev/ttyACM0' 
+BAUDRATE = 1000000
+TORQUE_ENABLE = 1
+TORQUE_DISABLE = 0
+portHandler = PortHandler(DEVICENAME)
+packetHandler = PacketHandler(PROTOCOL_VERSION)
+portHandler.openPort()
+portHandler.setBaudRate(BAUDRATE)
+for DXL_ID in DXL_IDS:
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_MARGIN, 0)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_MARGIN, 0)
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_SLOPE, 32)
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_SLOPE, 32)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 10)
+    # packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, 512)
+    # Read present position
+    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
+    if dxl_comm_result != COMM_SUCCESS:
+        print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+    elif dxl_error != 0:
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+    print("[ID:%03d]  PresPos:%03d" % (DXL_ID, dxl_present_position))
+
+for DXL_ID in DXL_IDS:
+    packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
+    
+
+
