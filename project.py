3e21022f979c4ff4b334a126281866f2cac169f3
s232909 <s232909@student.dtu.dk>
2024-11-08 11:50:59 +0100

    Update sample

--- project.py
+++ project.py
@@ -6,7 +6,7 @@ ADDR_MX_CCW_COMPLIANCE_MARGIN = 27
 ADDR_MX_CW_COMPLIANCE_SLOPE = 28
 ADDR_MX_CCW_COMPLIANCE_SLOPE = 29
 ADDR_MX_GOAL_POSITION = 30
-ADDR_MX_MOVING_SPEED = 15
+ADDR_MX_MOVING_SPEED = 32
 ADDR_MX_PRESENT_POSITION = 36
 ADDR_MX_PUNCH = 48
 PROTOCOL_VERSION = 1.0
@@ -25,7 +25,7 @@ for DXL_ID in DXL_IDS:
     packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_MARGIN, 0)
     packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CW_COMPLIANCE_SLOPE, 32)
     packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_CCW_COMPLIANCE_SLOPE, 32)
-    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 10)
+    packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 50)
     # packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_GOAL_POSITION, 512)
     # Read present position
     dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
